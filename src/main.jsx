5d2ffd833869a7b472e6cfb577b8cab9a84a78f8
monjurul <himel452@gmail.com>
2025-12-06 04:10:54 +0600

    Fixed index and main files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'  // <--- এই লাইনটি না থাকলে ডিজাইন আসবে না
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
